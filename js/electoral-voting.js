4e99e539581b5ab734953858482e3b990d7a1db4
Bruno Pedrazza <pedrazzabruno@gmail.com>
2020-10-24 23:04:25 -0300

    Add event to get the account updated

--- js/electoral-voting.js
+++ js/electoral-voting.js
@@ -1,17 +1,17 @@
-onload = function() {
-    if (!ethEnabled()) {
-        alert("Metamask or browser with Ethereum not detected!");
-    } else {
-        alert("Connected!");
-    }
-    console.log(web3.eth.accounts)
-}
-
-function ethEnabled() {
+const ethEnabled = () => {
     if (window.ethereum) {
         window.web3 = new Web3(window.ethereum);
         window.ethereum.enable();
         return true;
     }
     return false;
+}
+
+if (!ethEnabled()) {
+    alert("Metamask or browser with Ethereum not detected!");
+} else {
+    alert("Connected!");
+    window.ethereum.on('accountsChanged', function (accounts) {
+        console.log(accounts[0]);
+    });
 }
