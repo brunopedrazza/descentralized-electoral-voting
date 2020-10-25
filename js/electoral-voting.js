effe89aa2c495307b0bb6a6d3dffa3f047d6c202
Bruno Pedrazza <pedrazzabruno@gmail.com>
2020-10-25 14:06:31 -0300

    Split index html into a separated js fie

--- js/electoral-voting.js
+++ js/electoral-voting.js
@@ -1,62 +1,44 @@
+const contractABI = electoralVotingMetadata.output.abi;
+const contractByteCode = electoralVotingDeploy.data.bytecode.object;
 
-onload = function () {
-    var ethereumButton = document.getElementById('enableEthereumButton');
-    var accountAddress = document.getElementById('accountAddress');
+var contractToBeDeployed;
+var electoralVotingContract;
 
-    var electoralVotingABI = electoralVotingMetadata.output.abi
-    var electoralVotingByteCode = electoralVotingDeploy.data.bytecode.object
-
-    var accountSelected = '';
-
-    const ethEnabled = () => {
-        if (window.ethereum) {
-            window.web3 = new Web3(window.ethereum);
-            window.ethereum.enable();
-            return true;
-        }
-        return false;
+const ethEnabled = () => {
+    if (window.ethereum) {
+        window.web3 = new Web3(window.ethereum);
+        window.ethereum.enable();
+        return true;
     }
-    if (!ethEnabled()) {
-        alert("Metamask or browser with Ethereum not detected!");
-    }
-    else {
-        window.ElectoralVoting = new web3.eth.Contract(electoralVotingABI);
-        saveCoinbase();
-    }
+    return false;
+}
 
-    ethereumButton.addEventListener('click', () => {
-        getAccount();
-    });
+async function saveCoinbase() {
+    window.coinbase = await window.web3.eth.getCoinbase();
+};
 
-    async function getAccount() {
-        const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
-        accountSelected = accounts[0];
+if (!ethEnabled()) {
+    alert("Metamask or browser with Ethereum not detected!");
+}
+else {
+    contractToBeDeployed = new web3.eth.Contract(contractABI);
+    saveCoinbase();
+}
 
-        window.ElectoralVoting.deploy({
-            data: electoralVotingByteCode,
-            arguments: ['Presidencia', 'Brasil', '2020', 99999999999, 9999999999999]
-        })
-            .send({
-                from: accountSelected,
-                gas: 1500000,
-                gasPrice: '30000000000000'
-            }, function (error, transactionHash) { })
-            .on('error', function (error) { console.log(error) })
-            .on('transactionHash', function (transactionHash) { console.log(transactionHash) })
-            .on('receipt', function (receipt) {
-                console.log(receipt.contractAddress) // contains the new contract address
-            })
-            .on('confirmation', function (confirmationNumber, receipt) { console.log(confirmationNumber) })
-            .then(function (newContractInstance) {
-                console.log(newContractInstance.options.address) // instance with the new contract address
-            });
-
-        accountAddress.innerHTML = accountSelected;
-    }
-
-    ethereum.on('accountsChanged', function (accounts) {
-        accountSelected = accounts[0];
-        accountAddress.innerHTML = accountSelected;
-        // Time to reload your interface with accounts[0]!
-    });
+async function deployContract() {
+    var args = ['Presidencia', 'Brasil', '2020', 1603645200, 1603648800];
+    contractToBeDeployed.deploy({
+        data: contractByteCode,
+        arguments: args
+    }).send({
+        from: "0x0eec886B9b61aEdd64D1A308CC08a69Ee9f6DBA3",
+        gas: 3000000,
+        gasPrice: '1000000000'
+    }, function (error, transactionHash) { })
+        .on('error', function (error) { console.log(error); })
+        .on('transactionHash', function (transactionHash) { console.log(transactionHash); })
+        .on('confirmation', function (confirmationNumber, receipt) { console.log(confirmationNumber); })
+        .then(function (newContractInstance) {
+            electoralVotingContract = newContractInstance;
+        });
 }
