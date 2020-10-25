3510d798538ac2298fd1cd029bbfea53f65a49ff
Bruno Pedrazza <pedrazzabruno@gmail.com>
2020-10-25 11:33:28 -0300

    implement another way to deploy contract

--- js/electoral-voting.js
+++ js/electoral-voting.js
@@ -5,12 +5,24 @@ onload = function () {
 
     var electoralVotingABI = electoralVotingMetadata.output.abi
     var electoralVotingByteCode = electoralVotingDeploy.data.bytecode.object
-    var myContract = web3.eth.contract(electoralVotingABI);
-
-    console.log(web3.version);
-    console.log(myContract);
 
     var accountSelected = '';
+
+    const ethEnabled = () => {
+        if (window.ethereum) {
+            window.web3 = new Web3(window.ethereum);
+            window.ethereum.enable();
+            return true;
+        }
+        return false;
+    }
+    if (!ethEnabled()) {
+        alert("Metamask or browser with Ethereum not detected!");
+    }
+    else {
+        window.ElectoralVoting = new web3.eth.Contract(electoralVotingABI);
+        saveCoinbase();
+    }
 
     ethereumButton.addEventListener('click', () => {
         getAccount();
@@ -20,24 +32,24 @@ onload = function () {
         const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
         accountSelected = accounts[0];
 
-        myContract.deploy({
+        window.ElectoralVoting.deploy({
             data: electoralVotingByteCode,
             arguments: ['Presidencia', 'Brasil', '2020', 99999999999, 9999999999999]
         })
-        .send({
-            from: accountSelected,
-            gas: 1500000,
-            gasPrice: '30000000000000'
-        }, function(error, transactionHash){ })
-        .on('error', function(error){ console.log(error) })
-        .on('transactionHash', function(transactionHash){ console.log(transactionHash) })
-        .on('receipt', function(receipt){
-           console.log(receipt.contractAddress) // contains the new contract address
-        })
-        .on('confirmation', function(confirmationNumber, receipt){ console.log(confirmationNumber) })
-        .then(function(newContractInstance){
-            console.log(newContractInstance.options.address) // instance with the new contract address
-        });
+            .send({
+                from: accountSelected,
+                gas: 1500000,
+                gasPrice: '30000000000000'
+            }, function (error, transactionHash) { })
+            .on('error', function (error) { console.log(error) })
+            .on('transactionHash', function (transactionHash) { console.log(transactionHash) })
+            .on('receipt', function (receipt) {
+                console.log(receipt.contractAddress) // contains the new contract address
+            })
+            .on('confirmation', function (confirmationNumber, receipt) { console.log(confirmationNumber) })
+            .then(function (newContractInstance) {
+                console.log(newContractInstance.options.address) // instance with the new contract address
+            });
 
         accountAddress.innerHTML = accountSelected;
     }
