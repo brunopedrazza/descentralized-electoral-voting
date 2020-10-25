56212eb5f205c8af42fcf1ce258f38be48a3ee78
Bruno Pedrazza <pedrazzabruno@gmail.com>
2020-10-25 11:15:39 -0300

    Test to deploy contract

--- js/electoral-voting.js
+++ js/electoral-voting.js
@@ -17,6 +17,26 @@ onload = function () {
     async function getAccount() {
         const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
         accountSelected = accounts[0];
+
+        myContract.deploy({
+            data: electoralVotingByteCode,
+            arguments: ['Presidencia', 'Brasil', '2020', 99999999999, 9999999999999]
+        })
+        .send({
+            from: accountSelected,
+            gas: 1500000,
+            gasPrice: '30000000000000'
+        }, function(error, transactionHash){ })
+        .on('error', function(error){ console.log(error) })
+        .on('transactionHash', function(transactionHash){ console.log(transactionHash) })
+        .on('receipt', function(receipt){
+           console.log(receipt.contractAddress) // contains the new contract address
+        })
+        .on('confirmation', function(confirmationNumber, receipt){ console.log(confirmationNumber) })
+        .then(function(newContractInstance){
+            console.log(newContractInstance.options.address) // instance with the new contract address
+        });
+
         accountAddress.innerHTML = accountSelected;
     }
 
