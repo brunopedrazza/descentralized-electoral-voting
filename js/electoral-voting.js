aff736651fbb26252a699f657bfcb54dfaa344e4
Bruno Pedrazza <pedrazzabruno@gmail.com>
2020-10-25 10:31:35 -0300

    Implement getting bytecode and abi from smart contract

--- js/electoral-voting.js
+++ js/electoral-voting.js
@@ -1,43 +1,28 @@
-// import detectEthereumProvider from '@metamask/detect-provider';
+
 onload = function () {
-    var eth = new Eth(TestRPC.provider());
-    var el = function (id) { return document.getElementById(id); };
+    var ethereumButton = document.getElementById('enableEthereumButton');
+    var accountAddress = document.getElementById('accountAddress');
 
+    var electoralVotingABI = electoralVotingMetadata.output.abi
+    var electoralVotingByteCode = electoralVotingDeploy.data.bytecode.object
+    console.log(electoralVotingABI);
+    console.log(electoralVotingByteCode);
 
-    // uncomment to enable MetaMask support:
-    if (typeof window.web3 !== 'undefined' && typeof window.web3.currentProvider !== 'undefined') {
-        eth.setProvider(window.web3.currentProvider);
-    } else {
-        eth.setProvider(provider); // set to TestRPC if not available
+    var accountSelected = '';
+
+    ethereumButton.addEventListener('click', () => {
+        getAccount();
+    });
+
+    async function getAccount() {
+        const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+        accountSelected = accounts[0];
+        accountAddress.innerHTML = accountSelected;
     }
 
-
-    eth.accounts(function (accountsError, accounts) {
-        el('accountAddress').innerHTML = accounts[0].substr(0, 15);
-
-        // get the current account balance
-        eth.getBalance(accounts[0]).then(function (balance) {
-            el('accountBalance').innerHTML = Eth.fromWei(balance, 'ether');
-        });
+    ethereum.on('accountsChanged', function (accounts) {
+        accountSelected = accounts[0];
+        accountAddress.innerHTML = accountSelected;
+        // Time to reload your interface with accounts[0]!
     });
-    // var ethereumButton = document.getElementById('enableEthereumButton');
-    // var showAccount = document.getElementById('showAccount');
-
-    // var accountSelected = '';
-
-    // ethereumButton.addEventListener('click', () => {
-    //     getAccount();
-    // });
-
-    // async function getAccount() {
-    //     const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
-    //     accountSelected = accounts[0];
-    //     showAccount.innerHTML = accountSelected;
-    // }
-
-    // ethereum.on('accountsChanged', function (accounts) {
-    //     accountSelected = accounts[0];
-    //     showAccount.innerHTML = accountSelected;
-    //     // Time to reload your interface with accounts[0]!
-    // });
 }
