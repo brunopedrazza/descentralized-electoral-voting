e5a1a023f726a57caef2eda7c639a3ebab202bd6
Bruno Pedrazza <pedrazzabruno@gmail.com>
2020-10-25 09:02:39 -0300

    Add onload method to js

--- js/electoral-voting.js
+++ js/electoral-voting.js
@@ -1,20 +1,21 @@
 // import detectEthereumProvider from '@metamask/detect-provider';
+onload = function () {
+    var ethereumButton = document.getElementById('enableEthereumButton');
+    var showAccount = document.getElementById('showAccount');
 
-var ethereumButton = document.getElementById('enableEthereumButton');
-var showAccount = document.getElementById('showAccount');
+    ethereumButton.addEventListener('click', () => {
+        getAccount();
+    });
 
-ethereumButton.addEventListener('click', () => {
-  getAccount();
-});
+    async function getAccount() {
+        const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+        const account = accounts[0];
+        showAccount.innerHTML = account;
+    }
 
-async function getAccount() {
-  const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
-  const account = accounts[0];
-  showAccount.innerHTML = account;
+    ethereum.on('accountsChanged', function (accounts) {
+        const account = accounts[0];
+        showAccount.innerHTML = account;
+        // Time to reload your interface with accounts[0]!
+    });
 }
-
-ethereum.on('accountsChanged', function (accounts) {
-    const account = accounts[0];
-    showAccount.innerHTML = account;
-    // Time to reload your interface with accounts[0]!
-});
