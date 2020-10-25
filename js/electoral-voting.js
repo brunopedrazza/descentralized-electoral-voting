fa6fde84d7996c21af19bbd0146ca9181b51ae02
Bruno Pedrazza <pedrazzabruno@gmail.com>
2020-10-25 15:36:40 -0300

    Implement add candidate method properly

--- js/electoral-voting.js
+++ js/electoral-voting.js
@@ -15,33 +15,26 @@ const ethEnabled = () => {
 if (!ethEnabled()) {
     alert("Metamask or browser with Ethereum not detected!");
 }
+
 else {
     contractToBeDeployed = new web3.eth.Contract(contractABI);
     var accountInterval = setInterval(function () {
-        web3.eth.getAccounts()
-            .then(accounts => window.account = accounts[0]);
+        web3.eth.getAccounts().then(accounts => window.account = accounts[0]);
     }, 1000);
 }
 
 async function deployContract(politicalOffice, country, year, startTime, endTime) {
     var args = [politicalOffice, country, year, startTime, endTime];
-    contractToBeDeployed.deploy({
-        data: contractByteCode,
-        arguments: args
-    }).send({
-        from: window.account,
-        gas: 3000000,
-        gasPrice: '1000000000'
-    }, function () { })
+    contractToBeDeployed.deploy({ data: contractByteCode, arguments: args })
+        .send({ from: window.account, gas: 3000000, gasPrice: '1000000000' })
         .on('error', function (error) { console.log(error); })
         .on('transactionHash', function (transactionHash) { console.log(transactionHash); })
-        .then(function (newContractInstance) {
-            window.ElectoralVoting = newContractInstance;
-        });
+        .then(function (newContractInstance) { window.ElectoralVoting = newContractInstance; });
 }
 
 async function addCandidate(name, politicalParty, number) {
     var args = [name, politicalParty, number];
-    console.log(window.ElectoralVoting);
+    window.ElectoralVoting.methods.addCandidate(args).send({ from: window.account })
+        .on('receipt', function (receipt) { console.log(receipt); })
+        .on('error', function (error) { console.log(error); });
 }
-
