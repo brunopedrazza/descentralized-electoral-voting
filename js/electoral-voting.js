7b643a40c9649dd73c8c77426fdf5a9f0ce3ebf0
Bruno Pedrazza <pedrazzabruno@gmail.com>
2020-10-25 09:10:27 -0300

    Implement selected account

--- js/electoral-voting.js
+++ js/electoral-voting.js
@@ -3,18 +3,27 @@ onload = function () {
     var ethereumButton = document.getElementById('enableEthereumButton');
     var showAccount = document.getElementById('showAccount');
 
+    var selectedButton = document.getElementById('selectedButton');
+    var selectedAccount = document.getElementById('showSelectedAccount');
+
+    var accountSelected = '';
+
     ethereumButton.addEventListener('click', () => {
         getAccount();
     });
 
+    selectedButton.addEventListener('click', () => {
+        selectedAccount.innerHTML = accountSelected;
+    });
+
     async function getAccount() {
         const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
-        const account = accounts[0];
+        accountSelected = accounts[0];
         showAccount.innerHTML = account;
     }
 
     ethereum.on('accountsChanged', function (accounts) {
-        const account = accounts[0];
+        accountSelected = accounts[0];
         showAccount.innerHTML = account;
         // Time to reload your interface with accounts[0]!
     });
