3328b6cbff1107175afe233a4495c856958d26ca
Bruno Pedrazza <pedrazzabruno@gmail.com>
2020-10-25 21:28:51 -0300

    Implement handling

--- js/electoral-voting.js
+++ js/electoral-voting.js
@@ -27,7 +27,10 @@ async function deployContract(politicalOffice, country, year, startTime, endTime
     var args = [politicalOffice, country, year, startTime, endTime];
     contractToBeDeployed.deploy({ data: contractByteCode, arguments: args })
         .send({ from: window.account })
-        .on('error', function (error) { console.log(error); })
+        .on('error', function (error) {
+            handleError('deployContract');
+            console.log(error); 
+        })
         .on('transactionHash', function (transactionHash) { console.log(transactionHash); })
         .then(function (newContractInstance) {
             console.log('Contract deployed with success!');
@@ -37,62 +40,77 @@ async function deployContract(politicalOffice, country, year, startTime, endTime
 }
 
 async function addCandidate(name, politicalParty, number) {
-    var thash = '';
     window.ElectoralVoting.methods.addCandidate(name, politicalParty, number).send({ from: window.account })
         .on('receipt', function (receipt) {
-            console.log(receipt);
-            web3.eth.getTransaction(receipt.transactionHash)
-                .then(console.log);
+            console.log('Candidate added with success.');
+            console.log(receipt); 
         })
-        .on('transactionHash', function (transactionHash) { thash = transactionHash })
-        .on('error', function (error) {
-            web3.eth.getTransaction(thash)
-                .then(console.log);
+        .on('error', function (error) { 
+            handleError('addCandidate');
+            console.log(error); 
         });
 }
 
 async function getCandidate(number) {
     window.ElectoralVoting.methods.getCandidate(number).call({ from: window.account })
         .then(function (result) {
-            console.log(result);
+            console.log('Candidate found with success.');
+            console.log(result); 
         })
         .catch(function (error) {
-            console.log(error);
+            handleError('getCandidate');
+            console.log(error); 
         });
 }
 
 async function getVotesCount(number) {
     window.ElectoralVoting.methods.getCandidateVotesCount(number).call({ from: window.account })
         .then(function (result) {
-            console.log(result);
+            console.log('Number of votes for this candidate:');
+            console.log(result); 
         })
         .catch(function (error) {
-            console.log(error);
+            handleError('getVotesCount');
+            console.log(error); 
         });
 }
 
 async function vote(number) {
     window.ElectoralVoting.methods.vote(number).send({ from: window.account })
-        .on('receipt', function (receipt) { console.log(receipt); })
-        .on('error', function (error) { console.log(error); });
+        .on('receipt', function (receipt) {
+            console.log('Your vote was computed with success.');
+            console.log(receipt); 
+        })
+        .on('error', function (error) {
+            handleError('vote');
+            console.log(error); 
+        });
 }
 
 async function getElectionWinner() {
     window.ElectoralVoting.methods.getElectionWinner().call({ from: window.account })
         .then(function (result) {
-            console.log(result);
+            console.log('The winner was computed with success.');
+            console.log(result); 
         })
-        .catch(function (error) {
-            console.log(error);
+        .catch(function (error) { 
+            handleError('getElectionWinner');
+            console.log(error); 
         });
 }
 
 async function getMyVote() {
     window.ElectoralVoting.methods.getMyVote().call({ from: window.account })
         .then(function (result) {
-            console.log(result);
+            console.log('Your vote was returned with success.'); 
+            console.log(result); 
         })
         .catch(function (error) {
-            console.log(error);
+            handleError('getMyVote'); 
+            console.log(error); 
         });
+}
+
+function handleError(from){
+    console.log('There was an error: ' + from);
 }
