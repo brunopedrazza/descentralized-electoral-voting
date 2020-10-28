44fe350bfe4ed5c31ecb1db5f8515f2e3315f758
Bruno Pedrazza <pedrazzabruno@gmail.com>
2020-10-28 00:15:28 -0300

    Set handle revert to true

--- js/electoral-voting.js
+++ js/electoral-voting.js
@@ -9,6 +9,7 @@ const ethEnabled = () => {
     if (window.ethereum) {
         window.ethereum.autoRefreshOnNetworkChange = false;
         window.web3 = new Web3(window.ethereum);
+        window.web3.handleRevert = true;
         window.ethereum.request({ method: 'eth_requestAccounts' });
         return true;
     }
